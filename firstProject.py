ab6ee1e7e1a1bfb228c23360d11f8b2458ec5d10
Evan McCoy <evan.mccoy@gmail.com>
2019-04-16 15:30:16 -0400

    2 assignments for class

---
+++ firstProject.py
@@ -0,0 +1,5 @@
+firstName = input('What is your first name? ')
+lastName = input('What is your last name? ')
+color = input('And what is your favorite color? ')
+
+print("Hey %s %s! I like %s too" % (firstName.strip(), lastName, color))
---
+++ madlib.py
@@ -0,0 +1,6 @@
+animal = input("Name an exotic animal: ")
+number = input("Pick a number between 2 and 100: ")
+sport = input("What's your favorite sport? ")
+place = input("Name a place where people like to hang out: ")
+
+print("I saw %s %ss watching a %s game at the %s today" % (number, animal, sport, place))
